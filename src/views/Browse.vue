c14d1d2edb85adcd0e1a4330144072acb3166d2e
admin <vahe.khachatryan.y@tumo.org>
2020-01-09 17:00:28 +0400

    Map Made

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
   <div class="map-responsive">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d195168.32882273474!2d44.34814054026232!3d40.153305585270495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406aa2dab8fc8b5b%3A0x3d1479ae87da526a!2sYerevan%2C%20Armenia!5e0!3m2!1sen!2s!4v1578483982266!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+ <iframe src="https://www.google.com/maps/d/embed?mid=1LMt8faTipbwli79OXvEpeyX9e2sww7jM" width="100%" height="100%" frameborder="0" style="border:0"></iframe>
   </div>
   </div>
 </template>
@@ -13,15 +13,18 @@ export default {
   name: "App",
   components: {
     GoogleMap
-  }
-};
+  },
+  data: () => ({})
+}
 </script>
 
 <style>
 .map-responsive{
     overflow:hidden;
+    padding: 0;
     padding-bottom:56.25%;
     position:relative;
+    margin: 0;
     height:0;
 }
 .map-responsive iframe{
@@ -29,6 +32,8 @@ export default {
     top:0;
     height:100%;
     width:100%;
+    padding: 0;
+    margin: 0;
     position:absolute;
 }
 </style>
